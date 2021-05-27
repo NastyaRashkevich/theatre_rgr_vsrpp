2b5bcb1f788a54f9bede22bfe8e7ead99d9006ff
NastyaRashkevich <81294245+NastyaRashkevich@users.noreply.github.com>
2021-05-27 10:24:29 +0300

    rename

--- src/main/java/com/example/demo/Theatre.java
+++ src/main/java/com/example/demo/Theatre.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Theatre {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Theatre.class, args);
 	}
 
 }
